4d3e6e1f0e69964afcd37e580922871ade72e5d8
J-YSM <yeoshuiming@gmail.com>
2018-07-20 19:09:32 +0800

    added getting started scala,update gettingStarted with Head first java, added effective java summary

--- GettingStarted/src/GUI/MultipleListenersAndBorders.java
+++ GettingStarted/src/GUI/MultipleListenersAndBorders.java
@@ -68,7 +68,7 @@ public class MultipleListenersAndBorders {
     }
 
     /* inner classes can access class variables
-    *  valid use here since methods implementation not reusable */
+     *  valid use here since methods implementation not reusable */
     class LabelListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
@@ -88,27 +88,4 @@ public class MultipleListenersAndBorders {
         }
     }
 
-}
-
-class MyDrawPanel extends JPanel {
-
-    public void paintComponent(Graphics g) {
-
-        Graphics2D g2d = (Graphics2D) g;
-
-        int red = (int) (Math.random() * 255);
-        int green = (int) (Math.random() * 255);
-        int blue = (int) (Math.random() * 255);
-        Color startColor = new Color(red, green, blue);
-
-        red = (int) (Math.random() * 255);
-        green = (int) (Math.random() * 255);
-        blue = (int) (Math.random() * 255);
-        Color endColor = new Color(red, green, blue);
-
-        GradientPaint gradient = new GradientPaint(70, 70, startColor,
-                150, 150, endColor);
-        g2d.setPaint(gradient);
-        g2d.fillOval(70, 70, 100, 100);
-    }
 }
---
+++ ScalaGettingStarted/build.sbt
@@ -0,0 +1,5 @@
+name := "ScalaGettingStarted"
+
+version := "0.1"
+
+scalaVersion := "2.12.6"
